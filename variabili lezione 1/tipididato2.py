1faaf62218afa93bea7af811e1abf4222534bc30
andreanoceradev <andreanocera1995@gmail.com>
2026-02-07 15:41:00 +0100

    Create tipididato2.py
    tipididato2

---
+++ variabili lezione 1/tipididato2.py
@@ -0,0 +1,25 @@
+x = 5
+y = 6.5
+z = "ciao"
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+a = 10
+b = 1.8
+somma = a + b
+print(somma, type(somma))
+
+
+s1 = "Python"
+s2 = " è fantastico"
+frase = s1 + s2
+print(frase)
+
+
+
+x = 5
+y = 10
+print(x < 10 ) # true
+print(x == y ) # false
