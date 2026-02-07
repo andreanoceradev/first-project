ec701b73189a13d7e0132b5861e018838d490647
andreanoceradev <andreanocera1995@gmail.com>
2026-02-07 17:19:29 +0100

    Create casting.py
    esercizivariabili/casting.py

---
+++ variabili lezione 1/esercizivariabili/casting.py
@@ -0,0 +1,26 @@
+nome = "Andrea"
+età = 30 
+città = "Viterbo"
+print(nome,età,città)
+
+
+x = 2
+print(x)
+x = 9
+print(x)
+
+x = 10
+y = 50
+print("somma:",x + y)
+
+
+x = 200
+y = 500
+x, y = y, x
+print(x, y)
+
+
+base = 36
+altezza = 24
+area = base * altezza 
+print("l area del rettangolo è",area)
