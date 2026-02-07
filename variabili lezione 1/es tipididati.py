3878dc51c6df7dd34c056b5bb22ed144a80fa141
andreanoceradev <andreanocera1995@gmail.com>
2026-02-07 12:54:57 +0100

    tipididati

---
+++ variabili lezione 1/es tipididati.py
@@ -0,0 +1,15 @@
+x = 7
+y = 3
+print("somma" , x + y)
+
+prezzo = 19.99
+quantita = 3
+print("moltiplica " , prezzo * quantita)
+
+x = 10
+print(x>8) #True
+print(x>12) #False
+
+j = 5
+z = 2.5
+print( "somma", j + z )
