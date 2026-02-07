870dbf8d0de472f974be5281e0dc4ab51b132079
andreanoceradev <andreanocera1995@gmail.com>
2026-02-07 11:43:35 +0100

    Create esercizio variabile n 1.py
    esercizio variabili lezione 1

---
+++ variabili lezione 1/esercizio variabile n 1.py
@@ -0,0 +1,11 @@
+nome = "Andrea"
+print ("il mio nome è : Andrea")
+
+
+eta = int(input("Quanti anni hai?"))
+print("il doppio della tua età è :" ,eta * 2)
+
+
+num1 = 8
+num2 = 2
+print (" La somma è" ,num1 + num2)
