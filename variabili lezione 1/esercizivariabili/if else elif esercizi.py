a7418ed7203a531bcbb563347e85aac3d793efb0
andreanoceradev <andreanocera1995@gmail.com>
2026-03-14 12:36:36 +0100

    10 es. if, else, elif.py

---
+++ variabili lezione 1/esercizivariabili/if else elif esercizi.py
@@ -0,0 +1,83 @@
+#es 1
+
+x = - 10
+if x >= 0 :
+    print("positivo")
+else :
+    print("è negativo")
+
+#es 2
+
+    a = 8
+    b = 12
+    if a >= b:
+        print (" a è maggiore di b")
+    else:
+        print("b è maggiore di a")
+
+    #es 3
+
+    eta = 30 
+    if eta>=18 :
+        print ("sei maggiorenne")
+    else:
+        print ("sei minorenne")
+
+        #es 4
+
+    n = 8
+    if n % 2 == 0 :
+        print ("multiplo di 2")
+    else:
+        print("non è un multiplo di 2")
+
+        #es 5
+
+    voto = 17
+    if voto >= 18:
+            print( "esame superato")
+    else:
+            print("bocciato")
+
+        #es 6
+
+    c = "z"
+    if c in "aeiou" :
+            print ("vocale")
+    else:
+            print("consonante")
+    
+    #es 7
+
+    x= 0
+    if x >0:
+        print ("positivo")
+    elif x>0:
+        print ("negativo")
+    else:
+        print("zero")
+        
+
+        #es 8
+
+    età = 70 
+    if età <12 :
+        prezzo = 5
+    elif età <65:
+        prezzo = 10
+    else:
+        prezzo = 7
+    print("prezzo biglietto:" ,(prezzo) ,"€")
+
+
+#es 10
+
+a , b , c = 5 , 5 , 3
+if a==b == c:
+    print("triangolo equilatero")
+elif a==b or b==c or c==a:
+    print("triangolo isoscele")
+else:
+    print("triangolo scaleno")
+
+
